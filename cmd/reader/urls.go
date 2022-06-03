1bea6432f778b62d789a3cfbc6a0dd90e9d747cb
Choe, Cheng-Dae <whitekid@gmail.com>
2022-06-03 16:05:33 +0900

    add info/ update command

--- cmd/reader/urls.go
+++ cmd/reader/urls.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"strconv"
 
@@ -32,6 +34,21 @@ func init() {
 	})
 
 	cmd.AddCommand(&cobra.Command{
+		Use:   "update url",
+		Short: "update url",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			for _, url := range args {
+				log.Debugf("%s", url)
+				if _, err := reader.UpdateURL(cmd.Context(), url); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		},
+	})
+
+	cmd.AddCommand(&cobra.Command{
 		Use:   "list",
 		Short: "list urls",
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -47,6 +64,27 @@ func init() {
 				t.AppendRow([]interface{}{url.ID, reader.Shorten(url.ID), url.URL})
 			}
 			t.Render()
+			return nil
+		},
+	})
+
+	cmd.AddCommand(&cobra.Command{
+		Use:   "info id",
+		Short: "show url info",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			url, err := db.URL.ByID(uint(id))
+			if err != nil {
+				return err
+			}
+
+			v, _ := json.MarshalIndent(url, "", "  ")
+			fmt.Fprintf(os.Stdout, "%s", v)
+
 			return nil
 		},
 	})
