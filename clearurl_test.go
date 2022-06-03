6956f32701143ab6b1e3480f5b0526a3fbfebf88
Choe, Cheng-Dae <whitekid@gmail.com>
2022-06-03 15:47:29 +0900

    fix rules

--- clearurl_test.go
+++ clearurl_test.go
@@ -18,6 +18,7 @@ func TestCleanURL(t *testing.T) {
 		{"remove", args{"http://x.woosum.net/?utm_source=helloHelo09x&v=b"}, "http://x.woosum.net/?v=b"},
 		{"redirect", args{"https://blog.naver.com/businessinsight/222265339878"}, "https://m.blog.naver.com/businessinsight/222265339878"},
 		{"redirect", args{"https://m.blog.naver.com/PostView.naver?blogId=businessinsight&logNo=222222702267&redirect=Dlog&widgetTypeCall=true&directAccess=false"}, "https://m.blog.naver.com/businessinsight/222222702267"},
+		{"redirect", args{"https://m.blog.naver.com/PostView.nhn?blogId=businessinsight&logNo=221171804944&proxyReferer=http%3A%2F%2Fblog.naver.com%2Fbusinessinsight%2F221171804944"}, "https://m.blog.naver.com/businessinsight/221171804944"},
 		{"redirect", args{"https://infutureconsulting.tistory.com/1177"}, "https://infutureconsulting.tistory.com/m/1177"},
 		{"redirect", args{"https://infutureconsulting.tistory.com/m/1177"}, "https://infutureconsulting.tistory.com/m/1177"},
 		{"redirect", args{"https://infuture.kr/1177"}, "https://infutureconsulting.tistory.com/m/1177"},
